b5648cfe308d8edd49ab22579430334e403737d5
Rodrigo Tassinari de Oliveira <rodrigo@pittlandia.net>
2010-11-18 21:46:25 -0200

    env route for debugging

--- quick_tunes.rb
+++ quick_tunes.rb
@@ -22,12 +22,12 @@ get '/' do
    You're running a Sinatra application on Heroku!"
 end
 
-# Test at <appname>.heroku.com
+# Test at quicktunes.heroku.com
 
 # You can see all your app specific information this way.
 # IMPORTANT! This is a very bad thing to do for a production
 # application with sensitive information
 
-# get '/env' do
-#   ENV.inspect
-# end
+get '/env' do
+  ENV.inspect
+end
